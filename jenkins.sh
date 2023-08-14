d6031a8e01d94dfb34f3ce575eaac31327edb946
h4ckersmooth88 <h4ckersmooth88@gmail.com>
2023-08-14 14:22:05 +0700

    Add files via upload

---
+++ jenkins.sh
@@ -0,0 +1,1 @@
+docker run -u 0 --privileged --name jenkins -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v jenkins_home:/var/jenkins_home -v /usr/local/bin/kubectl:/usr/local/bin/kubectl hackersmooth88/dotnet
